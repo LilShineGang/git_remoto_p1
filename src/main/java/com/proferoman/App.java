3f8f5c28515b768cccf4e19abc0a45a35b3837c3
Blagovest Kroumov Doukov <blakrodou@alu.edu.gva.es>
2025-01-16 02:10:01 +0100

    Añadido mi nombre en Github

--- src/main/java/com/proferoman/App.java
+++ src/main/java/com/proferoman/App.java
@@ -2,20 +2,11 @@ package com.proferoman;
 
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Estos son los argumentos que has pasado:");
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
-
-        System.out.println("Fin del programa.");
-=======
         System.out.println("Listado de argumentos pasados al programa:");
         for (int i = 0; i < args.length; i++) {
             System.out.println("Argumento: " + (i + 1) + ": " + args[i]);
         }
 
         System.out.println("Fin del programa. Hasta pronto.");
->>>>>>> issue2
     }
 }
